7750d4266322a866424037733267781d9ba1bbd9
Minchul An <min@minchulan.io>
2022-08-14 18:34:56 -0400

    commit tree 1ba7cf1ffd7d6c20c6612618d3e4a45de8ee5a07

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  let result
+  if (someValue < 400) {
+    return "This one is on me!";
+  }
+  else if (someValue >= 400 && someValue < 2000) {
+    return "That will be twenty bucks.";
+  }
+  else if (someValue > 2000 && someValue <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (someValue > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(1500);
+scuberGreetingForFeet(2501);
+
+
+function ternaryCheckCity() {
+  if (ternaryCheckCity) {
+    return 'Ok, sounds good.'
+  }
 }
+
+ternaryCheckCity(NYC);
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
